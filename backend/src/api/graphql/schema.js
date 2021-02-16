7a551da42de999748e0f7ef6cc1af4ba41db88b0
Arturas Mickiewicz <artur.mickiewicz.vno@gmail.com>
2021-02-17 00:17:07 +0200

    persons, clinic & doctors on graphql

--- backend/src/api/graphql/schema.js
+++ backend/src/api/graphql/schema.js
@@ -4,13 +4,70 @@ const {
   GraphQLID,
   GraphQLSchema,
   GraphQLList,
+  GraphQLInt,
 } = require('graphql')
 
 const Product = require('../products/products.model')
+const Person = require('../persons/persons.model')
+const Clinic = require('../clinics/clinics.model')
+const Doctor = require('../doctors/doctors.model')
 
 const ProductType = new GraphQLObjectType({
   name: 'Product',
-  fields: () => ({ id: { type: GraphQLID }, name: { type: GraphQLString } }),
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  }),
+})
+
+const PersonType = new GraphQLObjectType({
+  name: 'Person',
+  fields: () => ({
+    id: { type: GraphQLID },
+    first: { type: GraphQLString },
+    last: { type: GraphQLString },
+    gender: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    address: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+})
+
+const ClinicType = new GraphQLObjectType({
+  name: 'Clinic',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    address: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+    web: { type: GraphQLString },
+  }),
+})
+
+const DoctorType = new GraphQLObjectType({
+  name: 'DoctorType',
+  fields: () => ({
+    id: { type: GraphQLID },
+    person: {
+      type: PersonType,
+      async resolve(parent, args) {
+        return await Person.query()
+          .where('deleted_at', null)
+          .findById(parent.person_id)
+      },
+    },
+    clinic: {
+      type: ClinicType,
+      async resolve(parent, args) {
+        return await Clinic.query()
+          .where('deleted_at', null)
+          .findById(parent.clinic_id)
+      },
+    },
+  }),
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -29,6 +86,45 @@ const RootQuery = new GraphQLObjectType({
         return await Product.query().where('deleted_at', null)
       },
     },
+    person: {
+      type: PersonType,
+      args: { id: { type: GraphQLID } },
+      async resolve(parent, args) {
+        return await Person.query().where('deleted_at', null).findById(args.id)
+      },
+    },
+    persons: {
+      type: new GraphQLList(PersonType),
+      async resolve(parent, args) {
+        return await Person.query().where('deleted_at', null)
+      },
+    },
+    clinic: {
+      type: ClinicType,
+      args: { id: { type: GraphQLID } },
+      async resolve(parent, args) {
+        return await Clinic.query().where('deleted_at', null).findById(args.id)
+      },
+    },
+    clinics: {
+      type: new GraphQLList(ClinicType),
+      async resolve(parent, args) {
+        return await Clinic.query().where('deleted_at', null)
+      },
+    },
+    doctor: {
+      type: DoctorType,
+      args: { id: { type: GraphQLID } },
+      async resolve(parent, args) {
+        return await Doctor.query().where('deleted_at', null).findById(args.id)
+      },
+    },
+    doctors: {
+      type: new GraphQLList(DoctorType),
+      async resolve(parent, args) {
+        return await Doctor.query().where('deleted_at', null)
+      },
+    },
   },
 })
 
@@ -37,7 +133,10 @@ const Mutation = new GraphQLObjectType({
   fields: {
     addProduct: {
       type: ProductType,
-      args: { name: { type: GraphQLString } },
+      args: {
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+      },
       async resolve(parent, args) {
         return await Product.query().insertAndFetch(args)
       },
